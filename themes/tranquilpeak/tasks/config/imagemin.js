3796c43ff20c1f4100b764d90ac3ebc9ad6cd5bb
ozagordi <osvaldo.zagordi+git@gmail.com>
2017-11-15 00:05:09 +0100

    Change text color/size, add some text, linted

--- themes/tranquilpeak/tasks/config/imagemin.js
+++ themes/tranquilpeak/tasks/config/imagemin.js
@@ -2,21 +2,20 @@ const mozjpeg = require('imagemin-mozjpeg');
 module.exports = function(grunt) {
   grunt.config.set('imagemin', {
     imagemin: {
-          options: {
-              optimizationLevel: 3,
-              svgoPlugins: [{removeViewBox: false}],
-              use: [mozjpeg()] // Example plugin usage
-          },
-            files: [{
-                expand: true,
-                cwd: 'src',
-                //src: ['**/*.{png,jpg,gif}'],
-                src: ['images/*.jpg'],
-                dest: 'dist/'
-            }]
-        }
+      options: {
+        optimizationLevel: 3,
+        svgoPlugins: [{removeViewBox: false}],
+        use: [mozjpeg()] // Example plugin usage
+      },
+      files: [{
+        expand: true,
+        cwd: 'src',
+        // src: ['**/*.{png,jpg,gif}'],
+        src: ['images/*.jpg'],
+        dest: 'dist/'
+      }]
+    }
   });
   grunt.loadNpmTasks('grunt-contrib-imagemin');
-//  grunt.registerTask('default', ['imagemin']);
-
+  //  grunt.registerTask('default', ['imagemin']);
 };
